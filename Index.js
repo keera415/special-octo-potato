d5c4e6b730a9fcc1e2221bf0b4f6d61d88c6787d
keera415 <Lt2kin1bases@gmail.com>
2025-05-05 01:47:41 +0400

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,14 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '/start') {
+        message.channel.send('Bot is now active!');
+    }
+});
+
+client.login(process.env.TOKEN);
