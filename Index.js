ea34eda1660f1a6b52365e0508ab5479c24ed4c2
keera415 <Lt2kin1bases@gmail.com>
2025-05-05 01:58:51 +0400

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,14 +1,22 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+require('dotenv').config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
 
 client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+  console.log(`✅ Logged in as ${client.user.tag}`);
 });
 
 client.on('messageCreate', message => {
-    if (message.content === '/start') {
-        message.channel.send('Bot is now active!');
-    }
+  if (message.content === '/start') {
+    message.channel.send('Bot is now active!');
+  }
 });
 
 client.login(process.env.TOKEN);
